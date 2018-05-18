9be4d778a3115e109fc3717ead373708cfa6bddd
golovanov_s <golovanov_s@IT-PC158>
2018-05-18 12:55:53 +0300

    main

--- ConsoleAppTestGit/Program.cs
+++ ConsoleAppTestGit/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleAppTestGit
         static void Main(string[] args)
         {
             Console.WriteLine("Привет мир!!!");
+            Console.ReadKey();
         }
     }
 }
